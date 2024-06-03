32de56e9573dee544d2dfd565bdadf219692dc80
rahmad <rhomadhoni.ra@gmail.com>
2024-06-03 20:00:46 +0700

    new elemen

--- app/src/main/java/com/msv/sejiwaku/MainActivity.kt
+++ app/src/main/java/com/msv/sejiwaku/MainActivity.kt
@@ -9,13 +9,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.msv.sejiwaku.homepage.journey.JourneyScreen
-import com.msv.sejiwaku.homepage.journey.menerimadiri.hirupnapasperlahan.HirupNafasPerlahan
 import com.msv.sejiwaku.homepage.journey.menerimadiri.MenerimaDiriScreen
-import com.msv.sejiwaku.homepage.konseling.detailscreen.DetailScreenKonseling
-import com.msv.sejiwaku.inJurnal.screen.journalScreen1
-import com.msv.sejiwaku.inJurnal.screen.journalScreen2
-import com.msv.sejiwaku.inJurnal.screen.journalScreen3
+import com.msv.sejiwaku.homepage.journey.menerimadiri.hirupnapasperlahan.HirupNafasPerlahan
+import com.msv.sejiwaku.homepage.journey.menerimadiri.musik.MusikStatu
 import com.msv.sejiwaku.navigator.OtakNavigasi
 import com.msv.sejiwaku.ui.theme.SejiwakuTheme
 
@@ -35,16 +31,14 @@ class MainActivity : ComponentActivity() {
                     //AppbardanBottonbar()
                     //LoginPage(navController = rememberNavController())
                     //BottonBarNavigation()
-                    OtakNavigasi(navController = rememberNavController())
+                    //OtakNavigasi(navController = rememberNavController())
                     //JournalScreen(navController = rememberNavController())
                     //JourneyScreen(navController = rememberNavController()){}
                     //DetailScreenKonseling(navController = rememberNavController())
                     //LinkLazyRow(urls = sampleUrls)
                     //MenerimaDiriScreen()
-                    //HirupNafasPerlahan()
-                    journalScreen1()
-                    journalScreen2()
-                    journalScreen3()
+                    //HirupNafasPerlahan(navController = rememberNavController())
+                    MusikStatu(navController = rememberNavController())
                 }
             }
         }
